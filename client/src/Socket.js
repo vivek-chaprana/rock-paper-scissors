4863a313be0e0991df638ec4a8490a2b39c2c30a
Vivek Chaprana <vivek2003ji@gmail.com>
2022-12-03 11:49:21 +0530

    Update Socket.js

--- client/src/Socket.js
+++ client/src/Socket.js
@@ -1,4 +1,4 @@
 import socketIo from "socket.io-client";
-const ENDPOINT = "https://rps-vicky.herokuapp.com/";
+const ENDPOINT = "https://rock-paper-scissors-o412.onrender.com";
 const socket = socketIo(ENDPOINT, { transports: ['websocket'] });
 export default socket;
